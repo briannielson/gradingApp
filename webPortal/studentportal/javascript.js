29f3301867dc2671dbc9b4e8db11aed93e27a22d
Paul Lamine <pjlamine@gmail.com>
2016-02-28 20:38:24 -0500

    Created JSON object for user input text, and changed var file to hold files

--- webPortal/studentportal/javascript.js
+++ webPortal/studentportal/javascript.js
@@ -1,18 +1,23 @@
 $(document).ready(function() {
   $("#submitButton").click(function(){
+
     if($('#userFile').val().length > 0)
     {
       $(':input[id="userInput"]').val(null);
-      var file = $('userFile').val();
+      var file = $("#userFile").get(0).files;
+
       var filename = $('input[type=file]').val().replace(/.*(\/|\\)/, '');
       var length = filename.length;
       var i = filename.lastIndexOf('.');
       var exten = filename.substring(i,length);
       if(!exten.match(".java")){
-        alert("This need to be a java file");
+        alert("This needs to be a java file");
       }
     }else{
       var text = $('#userInput').val();
+      // conversts text to JSON Object
+      var JSON_Text = JSON.stringify({text : text}, null, 1);
+
     }
   });
 });
