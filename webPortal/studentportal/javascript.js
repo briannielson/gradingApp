0d3ce3c7c5c38785e5a2696686f3be2545ca1d24
Noah Sibai <Noah Sibai>
2016-03-04 09:39:56 -0500

    This is the third and most recent version of the student portal

--- webPortal/studentportal/javascript.js
+++ webPortal/studentportal/javascript.js
@@ -1,18 +1,26 @@
 $(document).ready(function() {
+  $('#userFile').change(function(){
+      $(':input[id="userInput"]').val(null)//.slideUp();
+  });
   $("#submitButton").click(function(){
-    if($('#userFile').val().length > 0)
-    {
-      $(':input[id="userInput"]').val(null);
-      var file = $('userFile').val();
-      var filename = $('input[type=file]').val().replace(/.*(\/|\\)/, '');
-      var length = filename.length;
-      var i = filename.lastIndexOf('.');
-      var exten = filename.substring(i,length);
-      if(!exten.match(".java")){
-        alert("This need to be a java file");
-      }
-    }else{
-      var text = $('#userInput').val();
-    }
+    var jOb;
+    var x;
+          if($('#userFile').val().length > 0)
+          {
+            var file = $('#userFile').get(0).files;
+            var fileString = String(file);
+            var filename = $('input[type=file]').val().replace(/.*(\/|\\)/, '');
+            var length = filename.length;
+            var i = filename.lastIndexOf('.');
+            var exten = filename.substring(i,length);
+            if(!exten.match(".java")){
+              $(':input[id="userFile"]').val(null);
+              alert("This need to be a java file");
+            }
+            jOb = {"file":file};
+          }else{
+            var text = $('#userInput').val();
+            jOb ={"text":text};
+          }
   });
 });
