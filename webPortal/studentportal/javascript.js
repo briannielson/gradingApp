64f4267fdd92ea4b55e003cce53ddafed559c05b
Noah Sibai <Noah Sibai>
2016-03-18 14:04:15 -0400

    The newest version of the student portal 3-18-16

--- webPortal/studentportal/javascript.js
+++ webPortal/studentportal/javascript.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
           if($('#userFile').val().length > 0)
           {
             var file = $('#userFile').get(0).files;
-            var fileString = String(file);
             var filename = $('input[type=file]').val().replace(/.*(\/|\\)/, '');
             var length = filename.length;
             var i = filename.lastIndexOf('.');
@@ -17,9 +16,17 @@ $(document).ready(function() {
               alert("This need to be a java file");
             }
             jOb = {"file":file};
-          }else{
+           }else{
             var text = $('#userInput').val();
             jOb ={"text":text};
+            var today = new Date();
+            var expiry = new Date(today.getTime() + 30 * 24 * 3600 * 1000);
+            Cookies.set('textCookie',text);
+            if(Cookies.get().length() > 0){
+              console.log("yes");
+            }else{
+              console.log("no");
+            }
           }
-  });
+    });
 });
